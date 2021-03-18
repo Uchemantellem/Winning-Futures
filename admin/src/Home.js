8d0d6ba1bf42739511af47b9dbca5d3ccede46a4
uchemantellem <uchedeke@umich.edu>
2021-03-18 19:27:37 -0400

    some api stuff

--- admin/src/Home.js
+++ admin/src/Home.js
@@ -7,9 +7,28 @@ import 'react-form-builder2/dist/app.css';
 
 export class Home extends React.Component {
     constructor(props) {
-        super (props)
+        super(props);
+        this.apikey = ""; 
     }
 
+    componentDidMount() {
+        this.getforms();
+    }
+    getforms = () => {
+        var url = "https://tkjujrjyhdygpzy.form.io/successcoachform";
+        // let request = new Request(url, {method:"GET", headers: {"content-type": "application/json"}, body:'x-token='+this.apikey});
+        // console.log(request);
+        let params = {
+            "x-token": this.apikey,
+            'Content-Type': 'application/json'
+        };
+        fetch(url, {
+            method: 'GET',
+            headers: params
+        })
+            .then(response => response.text())
+            .then((response) => console.log(response));
+    }  
     render() {
         return(
             <h1>Placeholder Text Here</h1> 
